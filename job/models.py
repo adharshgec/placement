d1e2b8ae3e79e31024c0a0e63977fb600b79fc54
adharshgec <xskull03@gmail.com>
2017-05-13 18:30:15 +0530

    Resolved errors

--- job/models.py
+++ job/models.py
@@ -11,13 +11,14 @@ class Job(models.Model):
     date_of_test = models.DateField(blank=True, null=True)
     minimum_cgpa = models.DecimalField(max_digits=4, decimal_places=2)
     minimum_backlogs = models.IntegerField(default=1)
+    # applied_students = models.ManyToManyField(Profile)
 
     def __str__(self):
         return str(self.company_id) 
 
 class AppliedJob(models.Model):    
     student_id = models.CharField(max_length=30, blank=True)
-    student_applied_job = models.CharField(max_length=50, blank=True)
+    student_applied_job = models.CharField(max_length=50, blank=True)    
 
     def __str__(self):
         return str(self.student_applied_job)
